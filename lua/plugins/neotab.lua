04d8ac52cb0acb128c64e7f1479c271c15a52509
Cong Luan Tran <congluan.tran19@gmail.com>
2024-12-08 21:38:23 +0700

    add neotab

---
+++ lua/plugins/neotab.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "kawre/neotab.nvim",
+    event = "InsertEnter",
+    opts = {
+      tabkey = "<Tab>",
+      act_as_tab = true,
+      behavior = "nested", ---@type ntab.behavior
+      pairs = { ---@type ntab.pair[]
+        { open = "(", close = ")" },
+        { open = "[", close = "]" },
+        { open = "{", close = "}" },
+        { open = "'", close = "'" },
+        { open = '"', close = '"' },
+        { open = "`", close = "`" },
+        { open = "<", close = ">" },
+      },
+      exclude = {},
+      smart_punctuators = {
+        enabled = true,
+        semicolon = {
+          enabled = true,
+          ft = { "cs", "c", "cpp", "java" },
+        },
+        escape = {
+          enabled = false,
+          triggers = {}, ---@type table<string, ntab.trigger>
+        },
+      },
+    },
+  },
+}
